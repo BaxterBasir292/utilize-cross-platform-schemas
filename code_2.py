f6379488c29d2e6c19f76db950addd30871ef461
Basir <140265607+BaxterBasir292@users.noreply.github.com>
2023-11-15 17:59:57 +0100

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,77 +1,14 @@
-function main() {
-main();
-}
+    return data
 
-        data.push(Math.floor(Math.random() * 100) + 1);
-    data.forEach(item => {
-}
-    const data = generateRandomData();
-    const data = [];
-    return data;
-function generateRandomData() {
-        console.log("Random Number: " + item);
+    for item in data:
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
 
-    });
+def main():
 
-    for (let i = 0; i < 10; i++) {
-    }    });
-        data.push(Math.floor(Math.random() * 100) + 1);
-    return data;
 
-    data.forEach(item => {
-    const data = generateRandomData();
-main();
-function main() {
-}
-    }
-
-    const data = [];
-
-function generateRandomData() {
-}
-    for (let i = 0; i < 10; i++) {
-        console.log("Random Number: " + item);function generateRandomData() {
-    return data;
-    const data = generateRandomData();
-    for (let i = 0; i < 10; i++) {
-    data.forEach(item => {
-}
-    }
-        console.log("Random Number: " + item);
-
-        data.push(Math.floor(Math.random() * 100) + 1);
-
-main();
-    const data = [];
-    });
-
-}
-function main() {    const data = [];
-}
-
-    return data;
-        console.log("Random Number: " + item);
-        data.push(Math.floor(Math.random() * 100) + 1);
-    const data = generateRandomData();
-    for (let i = 0; i < 10; i++) {
-
-    }
-
-main();
-function main() {
-    data.forEach(item => {
-    });
-function generateRandomData() {
-}            data[i] = (int) (Math.random() * 100) + 1;
-    public static void main(String[] args) {
-        for (int item : data) {
-        int[] data = new int[10];
-        }
-public class RandomDataGenerator {
-}
-
-        }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Random Number: " + item);
-
-    }
+    data = generate_random_data()
+    main()
+if __name__ == "__main__":
+import random
+        print(f"Random Number: {item}")
